b53b482ea3758fe0679047499f1868e5f898f6d5
Maooghene Adheke <maooghenem@gmail.com>
2026-04-28 13:34:14 +0100

    updated

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const app = express()
 const port = process.env.PORT || 4000
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mern-auth-five-orpin.vercel.app/",
+];
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
